32020ef48c5ff11c7f8feef3e160e7a5126f5413
Ryan B <bombingrun3@gmail.com>
2025-10-14 20:51:21 -0500

    Cleaned up comments

--- app.js
+++ app.js
@@ -2,36 +2,19 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-// const stripe = require("stripe");
 // const { errors } = require("celebrate");
 const mainRouter = require("./routes/index");
 // const errorHandler = require("./middlewares/errorHandler");
-// const { loginUser, createUser } = require("./controllers/users");
 // const { requestLogger, errorLogger } = require("./middlewares/logger");
-// const {
-//   validateUserSignup,
-//   validateUserLogin,
-// } = require("./middlewares/validation");
 
 const app = express();
 
 const { PORT = 4000 } = process.env;
 
-// app.use(stripe("sk_test_your_secret_key_here"));
-
 // app.use(requestLogger);
 
 app.use(express.json());
 app.use(cors());
-
-// app.get("/crash-test", () => {
-//   setTimeout(() => {
-//     throw new Error("Server will crash now");
-//   }, 0);
-// });
-
-// app.post("/signup", validateUserSignup, createUser);
-// app.post("/signin", validateUserLogin, loginUser);
 
 app.use("/", mainRouter);
 
