722c9f5c95a0827952b9458ffe387abf9c3bb27a
Ryan B <bombingrun3@gmail.com>
2026-01-09 00:30:44 -0500

    Updated artItems to have multiple print sizes and prices

--- models/artItem.js
+++ models/artItem.js
@@ -1,60 +1,88 @@
 const mongoose = require("mongoose");
 
-const artItemSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-  },
-  categories: {
-    type: [String],
-    required: true,
-    validate: {
-      validator: (arr) =>
-        arr.length > 0 &&
-        arr.every((cat) =>
-          [
-            "landscape",
-            "abstract",
-            "people",
-            "pets",
-            "sketch",
-            "photo",
-            "print",
-          ].includes(cat)
-        ),
-      message: "Invalid category",
+const printOptionSchema = new mongoose.Schema(
+  {
+    size: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    available: {
+      type: Boolean,
+      default: true,
     },
   },
-  images: {
-    type: [String],
-    validate: [
-      {
-        validator: function (arr) {
-          return arr.length > 0;
-        },
-        message: "At least one image URL is required",
+  { _id: false }
+);
+
+const artItemSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+    },
+
+    categories: {
+      type: [String],
+      required: true,
+      validate: {
+        validator: (arr) =>
+          arr.length > 0 &&
+          arr.every((cat) =>
+            [
+              "landscape",
+              "abstract",
+              "people",
+              "pets",
+              "sketch",
+              "photo",
+              "print",
+            ].includes(cat)
+          ),
+        message: "Invalid category",
       },
-      {
-        validator: function (arr) {
-          return arr.every((url) => /^https?:\/\/.+/.test(url));
+    },
+
+    images: {
+      type: [String],
+      required: true,
+      validate: [
+        {
+          validator: (arr) => arr.length > 0,
+          message: "At least one image is required",
         },
-        message: "Invalid image URL",
+      ],
+    },
+
+    original: {
+      price: {
+        type: Number,
+        required: true,
       },
-    ],
+      dimensions: {
+        type: String,
+        required: true,
+      },
+      sold: {
+        type: Boolean,
+        default: false,
+      },
+    },
+
+    prints: {
+      type: [printOptionSchema],
+      default: [],
+    },
   },
-  original: {
-    price: { type: Number, required: true },
-    sold: { type: Boolean, default: false },
-    dimensions: { type: String, required: true },
-  },
-  print: {
-    price: { type: Number, required: true },
-    sold: { type: Boolean, default: false },
-    dimensions: { type: String, required: true },
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("ArtItem", artItemSchema);
